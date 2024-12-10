57a1a424158e8c8b43d34b0095b8ee30036cac24
Anas Farooq <anasfarooq2006@gmail.com>
2024-12-10 23:33:13 +0500

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -66,6 +66,7 @@ const Hero = () => {
         amount: "some",
         once: true,
       }}
+      
       className="mt-[55.5px] flex w-full flex-col items-center justify-center gap-6 md:flex-row lg:justify-start">
         <button className="group relative h-[56px] w-[180px] overflow-hidden rounded-full border-2 border-[#9984f0] text-base font-medium leading-[28px] text-[#445568] shadow-[0_1px_2px_0_rgba(14,24,41,0.05)] md:h-[60px] md:w-[202px] md:text-[18px]">
           <div className="absolute -left-1 bottom-0 right-0 -z-10 !mx-auto h-[0px] w-[207px] rounded-full bg-[#9984f0] transition-all duration-300 group-hover:h-[59px]"></div>
