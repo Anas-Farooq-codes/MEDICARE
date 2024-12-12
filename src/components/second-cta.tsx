9c86cbc3cb4c258645c527ec05502ef0f5089e03
Anas Farooq <anasfarooq2006@gmail.com>
2024-12-13 02:04:11 +0500

    Added Why Us Section

--- src/components/second-cta.tsx
+++ src/components/second-cta.tsx
@@ -4,6 +4,7 @@ const SecondCTA = () => {
   return (
     <section
       id="get-app"
+      className="relative w-full overflow-hidden px-6 py-10 md:px-10 md:py-[237px] xl:px-[159px]"
     >
       App
     </section>
